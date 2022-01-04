6a23d1eb63b3a114d35f09167c4171e61067b9bd
danielpine <danielpine@sina.com>
2022-01-04 17:39:04 +0800

    Completed order book.

--- coinexchange-ui/coin-portal/config/index.js
+++ coinexchange-ui/coin-portal/config/index.js
@@ -30,7 +30,7 @@ module.exports = {
   dev: {
     env: require('./dev.env'),
     port: 8081,
-    autoOpenBrowser: true,
+    autoOpenBrowser: false,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
